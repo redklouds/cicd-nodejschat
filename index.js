d6e4a841213ed23476f752ab146a3a08e1aaea38
Danny ly <dannyly199@gmail.com>
2018-01-12 21:51:51 -0800

    basic web service using express set up, foundation set

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ var express = require("express");
 var app = express();
 var port = 3700;
 
+//settings for view
+
+
 app.get("/", function(req, res){
-    res.send("it works!");
+    res.send("it works!" + req);
 });
 
-app.listen(port);
-console.log("Listening on port" + port);
+var server = app.listen(port, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log(server.address());
+	if(host =="::"){
+		host = "localhost";
+		console.log("localhost");
+	}
+	
+	console.log("Listening at http://%s:%s", host, port);
+	console.log("Press Ctrl+C to exit. . .");
+});
